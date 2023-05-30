f2ca299db49935a53bdbb46b4e8d5d55297722b8
Cristian Vergara Fernández <cristianvergaraf@gmail.com>
2023-05-31 00:35:25 +0200

    Update metricas.py
    corrigiendo error en el deploy

--- metricas.py
+++ metricas.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px 
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
